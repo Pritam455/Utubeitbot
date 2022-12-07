8c2713a6c7bf537f6eaadebe01b4cd3542fdda7e
Mafix3x <120046296+Mafix3x@users.noreply.github.com>
2022-12-07 20:17:57 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,19 +3,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN ="5877183056:AAF83LKb_9JEHEEkx5YFlINueM6VLnDfHxg"
 
-    SESSION_NAME = ":memory:"
+    SESSION_NAME =":memory:"
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID ="20620984"
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH ="7a710d252533a33b7db67fc42d62a1b6"
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID ="814700032498-710erok2296p0182aqraml1avkfmn0su.apps.googleusercontent.com"
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET ="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"
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER ="5956062507"
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
